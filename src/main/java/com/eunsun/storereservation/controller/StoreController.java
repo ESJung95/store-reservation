5058118d061921d5e7a2b9f4d08042a1276a33a6
eunsun <eunsunjj9501@gmail.com>
2024-05-03 01:51:09 +0900

    Store info update, delete function

--- src/main/java/com/eunsun/storereservation/controller/StoreController.java
+++ src/main/java/com/eunsun/storereservation/controller/StoreController.java
@@ -20,13 +20,40 @@ public class StoreController {
     private final StoreService storeService;
     private final JwtTokenProvider jwtTokenProvider;
 
-    // 매장 정보 수정
-    @PutMapping
+    // 매장 정보 삭제
+    @DeleteMapping("/{storeId}")
     @PreAuthorize("hasRole('ROLE_MANAGER')")
-    public ResponseEntity<?> modifyStoreInfo(@RequestBody StoreDto storeDto, Authentication authentication) {
+    public ResponseEntity<?> deleteStore(@PathVariable("storeId") Long storeId, Authentication authentication) {
+        try {
+            Long loginManagerId = jwtTokenProvider.extractManagerIdFromAuthentication(authentication);
+            storeService.deleteStore(storeId, loginManagerId);
+            log.info(storeId + "번 매장 정보 삭제 성공, 삭제 매니저 아이디 ->" + loginManagerId);
 
-        return ResponseEntity.ok().body("수정 성공");
+            return ResponseEntity.ok().build();
+
+        } catch (IllegalArgumentException e) {
+            log.error("매장 정보 삭제 실패 -> 권한 없음");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
     }
+    // 매장 정보 수정
+    @PutMapping("/{storeId}")
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    public ResponseEntity<?> modifyStoreInfo(@PathVariable("storeId") Long storeId, @RequestBody StoreDto storeDto, Authentication authentication) {
+        try {
+            Long loginManagerId = jwtTokenProvider.extractManagerIdFromAuthentication(authentication);
+            StoreDto updateStoreDto = storeService.modifyStoreInfo(storeId, storeDto, loginManagerId);
+            log.info("매장 정보 수정 성공 ->" + updateStoreDto);
+
+            return ResponseEntity.ok(updateStoreDto);
+
+        } catch (IllegalArgumentException e) {
+            log.error("매장 정보 삭제 실패 -> 권한 없음");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+    }
+
+
     // 매장 정보 저장
     @PostMapping
     @PreAuthorize("hasRole('ROLE_MANAGER')")
--- src/main/java/com/eunsun/storereservation/service/StoreService.java
+++ src/main/java/com/eunsun/storereservation/service/StoreService.java
@@ -19,7 +19,26 @@ public class StoreService {
     private final StoreRepository storeRepository;
     private final ManagerRepository managerRepository;
 
+    // 매장 정보 삭제
+    public void deleteStore(Long storeId, Long loginManagerId) {
+        Store store = storeRepository.findByIdAndManagerId(storeId, loginManagerId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 매장을 수정할 권한이 없습니다."));
+
+        storeRepository.deleteById(storeId);
+    }
     // 매장 정보 수정
+    public StoreDto modifyStoreInfo(Long storeId, StoreDto storeDto, Long loginManagerId) {
+        Store store = storeRepository.findByIdAndManagerId(storeId, loginManagerId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 매장을 수정할 권한이 없습니다."));
+
+        store.setStoreName(storeDto.getStoreName());
+        store.setLocation(storeDto.getLocation());
+        store.setDescription(storeDto.getDescription());
+        store.setStoreCallNumber(storeDto.getStoreCallNumber());
+
+        Store updatedStore = storeRepository.save(store);
+        return StoreDto.convertToDto(updatedStore);
+    }
 
     // 매장 정보 저장
     @Transactional
